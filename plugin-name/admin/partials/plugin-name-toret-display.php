394fdd365b2e4577b763430a58def243f3024c67
Jan Čejka <posta@jancejka.cz>
2017-05-01 12:17:30 +0200

    admin assets

---
+++ plugin-name/admin/partials/plugin-name-toret-display.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<div class="wrap">
+
+    <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+    <p>Přehled WordPress pluginů</p>
+
+</div>
